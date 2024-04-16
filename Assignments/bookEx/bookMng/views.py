96b6448d2abb394fa4a0b56abc576ad344960a6e
dayva001 <dayva001@users.noreply.github.com>
2024-04-15 18:24:52 -0700

    First commit
    uploaded all files from directory.

---
+++ Assignments/bookEx/bookMng/views.py
@@ -0,0 +1,113 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from bookMng.models import MainMenu, Book
+from .forms import BookForm
+
+# Create your views here.
+
+
+def index(request):
+    return render(request,
+                  'bookMng/index.html',
+                  {
+                      'item_list': MainMenu.objects.all()
+                  }
+                  )
+
+def home(request):
+    return render(request,
+                  'bookMng/home.html',
+                  {
+                      'item_list': MainMenu.objects.all()
+                  }
+                  )
+
+
+def displaybooks(request):
+    books = Book.objects.all()
+    for b in books:
+        b.pic_path = b.picture.url[14:]
+    return render(request,
+                  'bookMng/displaybooks.html',
+                  {
+                      'item_list': MainMenu.objects.all(),
+                      'book_list': books,
+                  }
+                  )
+
+
+def postbook(request):
+    submitted = False
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            book = form.save(commit=False)
+            try:
+                book.username = request.user
+            except Exception:
+                pass
+            book.save()
+            return HttpResponseRedirect('/postbook?submitted=True')
+    else:
+        form = BookForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request,
+                  'bookMng/postbook.html',
+                  {
+                      'form': form,
+                      'item_list': MainMenu.objects.all(),
+                      'submitted': submitted,
+                  }
+                  )
+
+
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    book.pic_path = book.picture.url[14:]
+    return render(request,
+                  'bookMng/book_detail.html',
+                  {
+                      'item_list': MainMenu.objects.all(),
+                      'book': book,
+                  }
+                  )
+
+def mybooks(request):
+    books = Book.objects.filter(username=request.user)
+    for b in books:
+        b.pic_path = b.picture.url[14:]
+    return render(request,
+                  'bookMng/mybooks.html',
+                  {
+                      'item_list': MainMenu.objects.all(),
+                      'book_list': books,
+                  }
+                  )
+
+def book_delete(request, book_id):
+    book = Book.objects.get(id=book_id)
+    book.delete
+    return render(request,
+                  'bookMng/mybooks.html',
+                  {
+                      'item_list': MainMenu.objects.all(),
+                      'book': book,
+                  }
+                  )
+
+class Register(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
+
+
+
