d48863107e6ca454117b3e448737c9721e15d51e
Lucky <63492687+kzhangg15@users.noreply.github.com>
2024-04-24 09:16:20 -0700

    Add files via upload

---
+++ bookEx/bookMng/views.py
@@ -0,0 +1,65 @@
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+from bookMng.models import MainMenu
+
+
+def index(request):
+    return render(request,
+                  'bookMng/index.html',
+                  {
+                      'item_list': MainMenu.objects.all()
+                  }
+                  )
+
+from .forms import BookForm
+
+
+def postbook(request):
+    submitted = False
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/postbook?submitted=True')
+    else:
+        form = BookForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request,
+                  'bookMng/postbook.html',
+                  {
+                      'form': form,
+                      'item_list': MainMenu.objects.all(),
+                      'submitted': submitted,
+                  }
+                  )
+
+from bookMng.models import Book
+
+def displaybooks(request):
+    books = Book.objects.all()
+    for b in books:
+        b.pic_path = b.picture.url[14:]
+    return render(request,
+                  'bookMng/displaybooks.html',
+                  {
+                      'item_list': MainMenu.objects.all(),
+                      'books': books
+                  }
+                  )
+
+
+class Register(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
